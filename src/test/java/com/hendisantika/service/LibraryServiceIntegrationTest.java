fe06e0cdde2db3b14528b38a62ce819c3981249b
Hendi Santika <hendisantika@gmail.com>
2021-10-24 21:29:55 +0700

    #17 - Add LibraryServiceIntegrationTest

---
+++ src/test/java/com/hendisantika/service/LibraryServiceIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.hendisantika.service;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import javax.inject.Inject;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-integration-test2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/10/21
+ * Time: 21.29
+ */
+@QuarkusTest
+class LibraryServiceIntegrationTest {
+
+    @Inject
+    LibraryService libraryService;
+
+}
