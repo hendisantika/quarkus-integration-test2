0ebe60b42274f62aa0b1b4fcf479d3b723da34c7
Hendi Santika <hendisantika@gmail.com>
2021-10-24 21:23:04 +0700

    #11 - Add findBooks API in LibraryResource

--- src/main/java/com/hendisantika/resource/LibraryResource.java
+++ src/main/java/com/hendisantika/resource/LibraryResource.java
@@ -1,12 +1,16 @@
 package com.hendisantika.resource;
 
+import com.hendisantika.model.Book;
 import com.hendisantika.service.LibraryService;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +29,9 @@ public class LibraryResource {
     @Inject
     LibraryService libraryService;
 
+    @GET
+    @Path("/book")
+    public Set<Book> findBooks(@QueryParam("query") String query) {
+        return libraryService.find(query);
+    }
 }
