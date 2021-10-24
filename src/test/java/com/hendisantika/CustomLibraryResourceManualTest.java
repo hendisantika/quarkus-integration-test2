e90b8a303594c162f4ddd5a297f622efdc1f9664
Hendi Santika <hendisantika@gmail.com>
2021-10-24 21:34:26 +0700

    #25 - Add whenGetBooksGivenNoQuery_thenAllBooksShouldBeReturned Test Case in CustomLibraryResourceManualTest

--- src/test/java/com/hendisantika/CustomLibraryResourceManualTest.java
+++ src/test/java/com/hendisantika/CustomLibraryResourceManualTest.java
@@ -3,6 +3,12 @@ package com.hendisantika;
 import com.hendisantika.util.CustomTestProfile;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +25,12 @@ class CustomLibraryResourceManualTest {
 
     public static final String BOOKSTORE_ENDPOINT = "/custom/library/book";
 
+    @Test
+    void whenGetBooksGivenNoQuery_thenAllBooksShouldBeReturned() {
+        given().contentType(ContentType.JSON)
+                .when().get(BOOKSTORE_ENDPOINT)
+                .then().statusCode(200)
+                .body("size()", is(2))
+                .body("title", hasItems("Foundation", "Dune"));
+    }
 }
