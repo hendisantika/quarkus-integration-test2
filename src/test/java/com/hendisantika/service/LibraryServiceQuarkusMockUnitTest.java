505765b7baaf962dfe5a4960d325955e8c6e7b58
Hendi Santika <hendisantika@gmail.com>
2021-10-24 21:31:44 +0700

    #21 - Add whenFindByAuthor_thenBooksShouldBeFound Test Case in LibraryServiceQuarkusMockUnitTest

--- src/test/java/com/hendisantika/service/LibraryServiceQuarkusMockUnitTest.java
+++ src/test/java/com/hendisantika/service/LibraryServiceQuarkusMockUnitTest.java
@@ -1,13 +1,17 @@
 package com.hendisantika.service;
 
+import com.hendisantika.model.Book;
 import com.hendisantika.repository.BookRepository;
 import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import javax.inject.Inject;
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,4 +37,11 @@ class LibraryServiceQuarkusMockUnitTest {
                         new Book("I Robot", "Isaac Asimov")}));
         QuarkusMock.installMockForType(mock, BookRepository.class);
     }
+
+    @Test
+    void whenFindByAuthor_thenBooksShouldBeFound() {
+        assertEquals(2, libraryService.find("Asimov").size());
+    }
 }
+
+
