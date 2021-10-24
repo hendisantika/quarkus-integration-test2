a34848ee8ace7c774743c32be6c3c66cb3c7328e
Hendi Santika <hendisantika@gmail.com>
2021-10-24 21:36:31 +0700

    #29 - Add whenGetBooksByTitle_thenBookShouldBeFound Test Case in LibraryResourceHttpResourceIntegrationTest

--- src/test/java/com/hendisantika/LibraryResourceHttpResourceIntegrationTest.java
+++ src/test/java/com/hendisantika/LibraryResourceHttpResourceIntegrationTest.java
@@ -4,8 +4,14 @@ import com.hendisantika.resource.LibraryResource;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
 
 import java.net.URL;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +29,13 @@ class LibraryResourceHttpResourceIntegrationTest {
     @TestHTTPResource("book")
     URL libraryEndpoint;
 
+    @Test
+    void whenGetBooksByTitle_thenBookShouldBeFound() {
+        given().contentType(ContentType.JSON).param("query", "Dune")
+                .when().get(libraryEndpoint)
+                .then().statusCode(200)
+                .body("size()", is(1))
+                .body("title", hasItem("Dune"))
+                .body("author", hasItem("Frank Herbert"));
+    }
 }
