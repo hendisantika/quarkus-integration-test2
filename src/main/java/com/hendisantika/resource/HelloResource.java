5cf7eea0456fb33a9ebfd04c75868f2956ba8dce
Hendi Santika <hendisantika@gmail.com>
2021-10-24 21:22:02 +0700

    #8 - Add greeting in HelloService

---
+++ src/main/java/com/hendisantika/resource/HelloResource.java
@@ -0,0 +1,35 @@
+package com.hendisantika.resource;
+
+import com.hendisantika.service.HelloService;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-integration-test2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/10/21
+ * Time: 21.19
+ */
+@Path("/hello")
+public class HelloResource {
+
+    @Inject
+    HelloService helloService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/polite/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return helloService.politeHello(name);
+    }
+
+
+}
