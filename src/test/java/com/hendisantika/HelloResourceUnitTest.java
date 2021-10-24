92523b332f32226b4a067241de14150ed58d3308
Hendi Santika <hendisantika@gmail.com>
2021-10-24 21:35:05 +0700

    #26 - Add HelloResourceUnitTest

---
+++ src/test/java/com/hendisantika/HelloResourceUnitTest.java
@@ -0,0 +1,29 @@
+package com.hendisantika;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-integration-test2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/10/21
+ * Time: 21.34
+ */
+@QuarkusTest
+public class HelloResourceUnitTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
+    }
+}
